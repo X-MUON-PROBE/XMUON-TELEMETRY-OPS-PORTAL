8152b72f7f7af73cfd415634a78fd540152ada10
eduard0sa <eduardoxaviersa@gmail.com>
2026-05-25 19:21:47 +0100

    PORTING MISSIONS PAGE TO REACT (STILL IN PROGRESS)

--- REACT_VERSION/src/components/missions.jsx
+++ REACT_VERSION/src/components/missions.jsx
@@ -1,8 +1,19 @@
 import { useState } from 'react'
-import './App.css'
+import '../App.css'
+import * as missionsScript from '../businessLogic/missions.js'
 
-function MissionsPage() {
-    return (
+/*
+ *{sessionLIST.map(session => (<tr>
+                    <td style={{padding: "20px"}}>{session.missioN_ID}</td>
+                    <td style={{padding: "20px"}}>{session.missioN_NAME}</td>
+                    <td style={{padding: "20px"}}>{session.missioN_START_TIMESTAMP}</td>
+                    <td style={{padding: "0px !important"}}><button class={openMissionButton} id={`openButton_${session}`}>ABRIR</button></td></tr>))}
+ * */
+
+async function MissionsPage() {
+	let missionData = await missionsScript.loadMissionsListData();
+	
+	return (
         <>
             <div class="missionsHero">
                 <h1>SESSÕES DE MEDIÇÃO:</h1>
@@ -16,7 +27,11 @@ function MissionsPage() {
                         <th>ABRIR SESSÃO</th>
                         </tr>
                     </thead>
-                    <tbody id="sessionList"></tbody>
+                    <tbody id="sessionList">{missionData.map(session => (<tr>
+                    <td style={{padding: "20px"}}>{session.missioN_ID}</td>
+                    <td style={{padding: "20px"}}>{session.missioN_NAME}</td>
+                    <td style={{padding: "20px"}}>{session.missioN_START_TIMESTAMP}</td>
+                    <td style={{padding: "0px !important"}}><button class="openMissionButton" id={`openButton_${session}`}>ABRIR</button></td></tr>))}</tbody>
                     </table>
                 </div>
             </div>
